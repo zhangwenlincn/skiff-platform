f35768aae6328c844b510cb7d235094a818d9d0d
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-08-12 14:25:13 +0800

    换一种BeanUtil实现

--- skiff-commons/skiff-common-core/src/main/java/com/skiff/common/core/util/BeanUtil.java
+++ skiff-commons/skiff-common-core/src/main/java/com/skiff/common/core/util/BeanUtil.java
@@ -1,36 +1,23 @@
 package com.skiff.common.core.util;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
+import org.modelmapper.ModelMapper;
 
-import java.lang.reflect.Constructor;
 import java.util.List;
 
 public class BeanUtil {
 
-    private static final Logger log = LoggerFactory.getLogger(BeanUtil.class);
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static ModelMapper getInstance() {
+        return modelMapper;
+    }
 
     public static <T> T copy(Object source, Class<T> targetClass) {
-        try {
-            Constructor<T> declaredConstructor = targetClass.getDeclaredConstructor();
-            declaredConstructor.setAccessible(true);
-            T result = declaredConstructor.newInstance();
-            BeanUtils.copyProperties(source, result);
-            return result;
-        } catch (Exception e) {
-            log.error("类属性复制异常", e);
-            throw new RuntimeException("类属性复制异常");
-        }
+        return BeanUtil.getInstance().map(source, targetClass);
     }
 
     public static <T> List<T> copyList(List<Object> sourceList, Class<T> targetClass) {
-        try {
-            return sourceList.stream().map(source -> copy(source, targetClass)).toList();
-        } catch (Exception e) {
-            log.error("类集合属性复制异常", e);
-            throw new RuntimeException("类集合属性复制异常");
-        }
+        return sourceList.stream().map(source -> copy(source, targetClass)).toList();
     }
 }
