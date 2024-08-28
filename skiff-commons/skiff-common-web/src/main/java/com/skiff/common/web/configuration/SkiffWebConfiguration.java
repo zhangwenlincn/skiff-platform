23eaf97ec5ecdb14ee79c53c5e05ce5f8eb687c7
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-08-28 16:38:18 +0800

    format json

--- skiff-commons/skiff-common-web/src/main/java/com/skiff/common/web/configuration/SkiffWebConfiguration.java
+++ skiff-commons/skiff-common-web/src/main/java/com/skiff/common/web/configuration/SkiffWebConfiguration.java
@@ -1,5 +1,7 @@
 package com.skiff.common.web.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skiff.common.core.util.JsonUtil;
 import com.skiff.common.web.exception.SkiffExceptionHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +12,10 @@ public class SkiffWebConfiguration {
     @Bean
     public SkiffExceptionHandler skiffExceptionHandler() {
         return new SkiffExceptionHandler();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonUtil.getObjectMapper();
     }
 }
