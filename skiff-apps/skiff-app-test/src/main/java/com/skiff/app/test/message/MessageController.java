e2ed979f2ddb05189e55e8367c78bf9d047d896d
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-09-06 16:07:41 +0800

    test

--- skiff-apps/skiff-app-test/src/main/java/com/skiff/app/test/message/MessageController.java
+++ skiff-apps/skiff-app-test/src/main/java/com/skiff/app/test/message/MessageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/message")
 public class MessageController {
 
     @Resource
