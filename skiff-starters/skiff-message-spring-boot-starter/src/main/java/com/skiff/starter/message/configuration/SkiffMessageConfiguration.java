fd6e5f0ddad516f14db08b19377b4900aaf24805
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-09-06 10:49:32 +0800

    EnableMessage

--- skiff-starters/skiff-message-spring-boot-starter/src/main/java/com/skiff/starter/message/configuration/SkiffMessageConfiguration.java
+++ skiff-starters/skiff-message-spring-boot-starter/src/main/java/com/skiff/starter/message/configuration/SkiffMessageConfiguration.java
@@ -1,9 +1,9 @@
 package com.skiff.starter.message.configuration;
 
-import com.skiff.common.message.register.MessageActuatorRegister;
 import com.skiff.common.message.storage.MessageStorageService;
 import com.skiff.common.message.timer.DefaultMessageActuatorTask;
 import com.skiff.common.message.timer.MessageActuatorTask;
+import com.skiff.starter.message.annotation.EnableMessage;
 import com.skiff.starter.message.register.SpringMessageActuatorRegister;
 import org.slf4j.Logger;
 import org.springframework.beans.BeansException;
@@ -13,8 +13,12 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SkiffMessageConfiguration implements ApplicationContextAware {
@@ -38,9 +42,18 @@ public class SkiffMessageConfiguration implements ApplicationContextAware {
     public MessageActuatorTask defaultMessageActuatorTask() {
         SpringMessageActuatorRegister register = new SpringMessageActuatorRegister(applicationContext);
         Map<String, Object> startMap = applicationContext.getBeansWithAnnotation(SpringBootApplication.class);
-        List<String> packages = startMap.values().stream().map(x -> x.getClass().getPackage().getName()).toList();
-        register.register(packages.toArray(new String[0]));
-        logger.info("Message actuator register completed. register packages: {}", packages);
+        List<String> packages = startMap.values().stream().map(x -> x.getClass().getPackage().getName()).collect(Collectors.toList());
+        Map<String, Object> messageDefultMap = applicationContext.getBeansWithAnnotation(EnableMessage.class);
+        if (!messageDefultMap.isEmpty()) {
+            packages.addAll(messageDefultMap.values().stream().map(x -> x.getClass().getPackage().getName()).toList());
+            packages.addAll(messageDefultMap.values().stream().map(x -> {
+                EnableMessage annotation = x.getClass().getAnnotation(EnableMessage.class);
+                return Arrays.stream(annotation.scanBasePackages()).toList();
+            }).flatMap(Collection::stream).distinct().filter(Objects::nonNull).toList());
+        }
+        List<String> actualPackages = packages.stream().distinct().collect(Collectors.toList());
+        register.register(actualPackages.toArray(new String[0]));
+        logger.info("Message actuator register completed. register packages: {}", actualPackages);
         MessageActuatorTask task = new DefaultMessageActuatorTask(messageStorageService);
         task.schedule();
         return task;
