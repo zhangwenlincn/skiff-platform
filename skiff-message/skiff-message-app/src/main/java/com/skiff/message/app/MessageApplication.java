a5342aedcc716a8fb9f393bcb718142acc7c8a2f
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-10-21 09:46:19 +0800

    MessageApplication

---
+++ skiff-message/skiff-message-app/src/main/java/com/skiff/message/app/MessageApplication.java
@@ -0,0 +1,11 @@
+package com.skiff.message.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
+}
